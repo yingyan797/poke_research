60feb79c79736e0837116747a7a034ef3868e532
Andy Chen <andy.chen121@imperial.ac.uk>
2025-07-10 02:53:54 +0100

    pokemon query tools hierarchy

--- pokemon_info.py
+++ pokemon_info.py
@@ -1,39 +1,78 @@
 import pokebase as pb
+from pokebase import type_
 
 ATTR_QUERY_KEYS = {
-    pb.type_: ['normal', 'fighting', 'flying', 'poison', 'ground', 'rock',
+    "have the type": (type_, ['normal', 'fighting', 'flying', 'poison', 'ground', 'rock',
         'bug', 'ghost', 'steel', 'fire', 'water', 'grass',
         'electric', 'psychic', 'ice', 'dragon', 'dark', 'fairy',
-        'unknown', 'shadow'],
-    pb.pokemon_habitat: ['cave', 'forest', 'grassland', 'mountain', 'rare',
-        'rough-terrain', 'sea', 'urban', 'waters-edge'],
-    pb.pokemon_color: ['black', 'blue', 'brown', 'gray', 'green', 'pink',
-        'purple', 'red', 'white', 'yellow'],
-    pb.egg_group: ['monster', 'water1', 'bug', 'flying', 'field',
+        'unknown', 'shadow']),
+    "lives in habitat": (pb.pokemon_habitat, ['cave', 'forest', 'grassland', 'mountain', 'rare',
+        'rough-terrain', 'sea', 'urban', 'waters-edge']),
+    "have color": (pb.pokemon_color, ['black', 'blue', 'brown', 'gray', 'green', 'pink',
+        'purple', 'red', 'white', 'yellow']),
+    "belong to egg group": (pb.egg_group, ['monster', 'water1', 'bug', 'flying', 'field',
         'fairy', 'grass', 'human-like', 'water3', 'mineral',
-        'amorphous', 'water2', 'ditto', 'dragon', 'undiscovered'],
-    pb.pokemon_shape: ['ball', 'squiggle', 'fish', 'arms', 'blob', 'upright',
+        'amorphous', 'water2', 'ditto', 'dragon', 'undiscovered']),
+    "have shape": (pb.pokemon_shape, ['ball', 'squiggle', 'fish', 'arms', 'blob', 'upright',
         'legs', 'quadruped', 'wings', 'tentacles',
-        'heads', 'humanoid', 'bug-wings', 'armor'],
-    pb.growth_rate: ['slow', 'medium', 'fast', 'medium-slow', 'slow-then-very-fast', 'fast-then-very-slow']
+        'heads', 'humanoid', 'bug-wings', 'armor']),
+    "with growth rate": (pb.growth_rate, ['slow', 'medium', 'fast', 'medium-slow', 'slow-then-very-fast', 'fast-then-very-slow']),
+    "have the ability": (pb.ability, ['levitate', 'blaze', 'torrent', 'overgrow', 'pressure',
+        'intimidate', 'static', 'inner-focus', 'synchronize',
+        'poison-point', 'chlorophyll', 'huge-power']),
+    "learn the move": (pb.move, ['tackle', 'ember', 'water-gun', 'psychic', 'thunderbolt',
+        'earthquake', 'ice-beam', 'protect', 'rest'])
 }
 
-def pokemon_single_query(query_func, keys):
-    values = set()
-    for k in keys:
-        names = [p.pokemon.name for p in query_func(k).pokemon] if query_func is pb.type_ else [p.name for p in query_func(k).pokemon_species]
-        values = values.union(names)
-    return values
+class QueryTool:
+    def __init__(self):
+        pass
+    def all_keys(self):
+        return [r.name for r in self.query_func("").results]    
 
-def pokemon_cross_query(query_list):
-    values = None
-    for query_func, keys in query_list:
-        if values is not None:
-            values = values.intersection(pokemon_single_query(query_func, keys))
-        else:
-            values = pokemon_single_query(query_func, keys)
-    return values
+class PokemonAttributeQuery(QueryTool):
+    def __init__(self, attr, keys=[]):
+        self.query_func = ATTR_QUERY_KEYS[attr][0]
+        self.keys = keys
+
+    def find_pokemon(self):            
+        return {key: [value.name for value in self.query_func(key).pokemon_species] for key in self.keys}
+    
+class PokemonTypeQuery(PokemonAttributeQuery):
+    def __init__(self, keys=[]):
+        self.query_func = type_
+        super().__init__("have the type", keys)
+    def find_pokemon(self):
+        return {key: [value.pokemon.name for value in type_(key).pokemon] for key in self.keys}
+    
+    @staticmethod
+    def _dr_info(damage_relations):
+        return {drk: [drv.name for drv in drvs] for drk, drvs in damage_relations.__dict__.items()}
+        
+    def damage_relations(self):
+        return {key: PokemonTypeQuery._dr_info(type_(key).damage_relations) for key in self.keys}
+    def game_indices(self):
+        return {key: [{"game_index": gi.game_index, "generation": gi.generation.name} for gi in type_(key).game_indices] for key in self.keys}
+    def generation(self):
+        return {key: type_(key).generation.name for key in self.keys}
+    def find_moves(self):
+        return {key: [value.name for value in type_(key).moves] for key in self.keys}
+    def move_damage_class(self):
+        return {key: type_(key).move_damage_class.name for key in self.keys}
+    def past_damage_relations(self):
+        return {key: [{"damage_relations": PokemonTypeQuery._dr_info(pdr.damage_relations), "generation": pdr.generation.name} for pdr in type_(key).past_damage_relations] for key in self.keys}
+
+class MoveQuery(PokemonAttributeQuery):
+    def __init__(self, keys=[]):
+        self.query_func = pb.move
+        super().__init__("learn the move", keys)
+    def find_pokemon(self):
+        return {key: [value.name for value in pb.move(key).learned_by_pokemon] for key in self.keys}
         
 if __name__ == "__main__":
-    print(pokemon_cross_query([(pb.pokemon_shape, ["ball"]), (pb.type_, ["grass"])]))
-    # print(pokemon_single_query(pb.type_, ["water"]))
+    # print(pokemon_and_query([("type", ["grass"]), ("move", ["tackle"])]))
+    # print(pokemon_single_query("learn the move", ["tackle"]))
+    # print(pb.pokemon_habitat("").results)
+    # pqr = MoveQuery(["tackle"])
+    pqr = PokemonTypeQuery(["fire"])
+    print(pqr.past_damage_relations())
